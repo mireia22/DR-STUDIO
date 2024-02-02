e1ebf43fa39c418faa48f61140bb4bfde0bfd9d3
mireia22 <mireiagarciaferrer22@gmail.com>
2024-02-02 06:59:12 +0100

    upload all projects

--- src/constants.ts
+++ src/constants.ts
@@ -16,7 +16,7 @@ export const PROJECTS: ProjectType[] = [
     ],
     description: `Trabajo para exposición de trabajos de exalumnos. El concepto de l'exposición seria SENCILLEZ, y su nombre hacee referencia a seguir adelante con los conocimientos adquiridos. `,
     category: "interior",
-    pdf: "/AVANT.pdf",
+    pdf: ".../avant/AVANT.pdf",
     color: "#0183aa",
   },
   {
@@ -58,7 +58,7 @@ export const PROJECTS: ProjectType[] = [
     ],
     description: `Propuesta de remodelación de una de las plantas de CosmoCaixa. Los hexagonos són elementos protagonistas en el diseño, aportando referencias naturales. `,
     category: "interior",
-    pdf: "/COSMOCAIXA.pdf",
+    pdf: ".../cosmocaixa/COSMOCAIXA.pdf",
     color: "#1d1d1d",
   },
   {
@@ -93,7 +93,7 @@ export const PROJECTS: ProjectType[] = [
     l'amo. Com a vegetació s'ha fet una recerca
     de vegetació autòctona i hem fet una selecció de la natura que voldríem aplicar.`,
     category: "interior",
-    pdf: "/HABITACOLA.pdf",
+    pdf: ".../habitacola/HABITACOLA.pdf",
     color: "#1d1d1d",
     video: "../public/habitacola/Cal mero.mp4",
   },
@@ -123,7 +123,253 @@ export const PROJECTS: ProjectType[] = [
     Aquesta intervenció està molt relacionada amb l’espai gràcies a la corda que ens relaciona l’intervenció amb el rocòdrom situat en aquesta
     ubicació.`,
     category: "exterior",
-    pdf: "/LLUERNIA.pdf",
+    pdf: ".../lluernia/LLUERNIA.pdf",
     color: "#ffe001",
+  },
+  {
+    id: "6",
+    title: "Logotipo Imagen Corporativa",
+    mainImage:
+      "../logotip-centresocial/virtual/virtual-logotip-centresocial-1.png",
+    virtualImages: [
+      "../logotip-centresocial/virtual/virtual-logotip-centresocial-1.png",
+      "../logotip-centresocial/virtual/virtual-logotip-centresocial-2.png",
+      "../logotip-centresocial/virtual/virtual-logotip-centresocial-3.png",
+    ],
+    tecnicImages: [
+      "../logotip-centresocial/planols/planol-logotip-centresocial-1.png",
+      "../logotip-centresocial/planols/planol-logotip-centresocial-2.png",
+    ],
+
+    description: `Propuesta de imagen corporativa 
+    para Centre Social El Roser. La intervención 
+    esta fonamentada con los atributos y filosofia 
+    del centro: integración social, 
+    reaprovechamiento, sostenibilidad, empoderación
+     de las personas y ayuda para la inserción 
+     efectiva. El logotipo hace referencia al 
+     pasillo que conecta el comedor con el 
+     edificio
+      de las entidades. La imagen tiene mucho 
+      peso visual y representa la parte 
+      antigua del edificio, haciendo referencia 
+      a las piedras de l'exterior. `,
+    category: "exterior",
+    pdf: ".../logotip-centresocial/ELROSER.pdf",
+    color: "#2d2a26",
+  },
+  {
+    id: "7",
+    title: "Exposición PFC",
+    mainImage: "../pfc-expo/virtual/virtual-pfc-1.png",
+    virtualImages: [
+      "../pfc-expo/virtual/virtual-pfc-1.png",
+      "../pfc-expo/virtual/virtual-pfc-2.png",
+      "../pfc-expo/virtual/virtual-pfc-3.png",
+    ],
+    tecnicImages: [
+      "../pfc-expo/planols/planol-pfc-1.png",
+      "../pfc-expo/planols/planol-pfc-2.png",
+      "../pfc-expo/planols/planol-pfc-3.png",
+    ],
+    realImages: [
+      "../pfc-expo/real/real-pfc-1.png",
+      "../pfc-expo/real/real-pfc-2.png",
+      "../pfc-expo/real/real-pfc-3.png",
+      "../pfc-expo/real/real-pfc-4.png",
+      "../pfc-expo/real/real-pfc-5.png",
+      "../pfc-expo/real/real-pfc-6.png",
+      "../pfc-expo/real/real-pfc-7.png",
+      "../pfc-expo/real/real-pfc-8.png",
+      "../pfc-expo/real/real-pfc-9.png",
+      "../pfc-expo/real/real-pfc-10.png",
+    ],
+
+    description: `El concepto de la exposición consiste a crear un eespacio on todo lo que se 
+    expone esta desarrollado en formatos que no son habituales, como són las lonas, que se repiten en diferentes
+    zonas. De esta forma, toda la exposición está ligada por su formato material....
+    
+    
+    `,
+    category: "interior",
+    pdf: ".../pfc-expo/PFC.pdf",
+    color: "#e99394",
+  },
+  {
+    id: "8",
+    title: "Diseño plato TV",
+    mainImage: "../platotv/virtual/virtual-tv-6.png",
+
+    virtualImages: [
+      "../platotv/virtual/virtual-tv-1.png",
+      "../platotv/virtual/virtual-tv-2.png",
+      "../platotv/virtual/virtual-tv-3.png",
+      "../platotv/virtual/virtual-tv-4.png",
+      "../platotv/virtual/virtual-tv-5.png",
+      "../platotv/virtual/virtual-tv-6.png",
+    ],
+    tecnicImages: ["../platotv/planols/planol-tv-1.png"],
+
+    description: `
+    Propuesta de diseño de un plato de TV para un Late Night.
+
+    `,
+    category: "interior",
+    pdf: ".../platotv/TV.pdf",
+    color: "#e00e16",
+  },
+  {
+    id: "9",
+    title: "Interior Tienda Urbana",
+    mainImage: "../redskate-store/virtual/virtual-redskate-store-2.png",
+
+    virtualImages: [
+      "../redskate-store/virtual/virtual-redskate-store-1.png",
+      "../redskate-store/virtual/virtual-redskate-store-2.png",
+      "../redskate-store/virtual/virtual-redskate-store-3.png",
+    ],
+    tecnicImages: [
+      "../redskate-store/planols/planol-redskate-store-1.png",
+      "../redskate-store/planols/planol-redskate-store-2.png",
+    ],
+
+    description: `
+    
+    Es tracta d'una botiga de dona basada en l'Urban, ja que
+    va molt amb el projecte creat.
+    
+    És un projecte creat a partir de malles metàl·liques perfora-
+    des, i anar jugant amb diferents tipus de perforat. He utilit-
+    zat diferents materials industrials, que es basaria en tot un
+    
+    interior basat en materials industrials, juntament amb
+    llums neó i vidres, i així jugar amb les vistes de l'interior, ja
+    sigui pels miralls, ampliant els interiors, com per fer un
+    espai més ampli.
+    Els materials emprats són fusta, metalls i vidres. La fusta
+    està usada en forma de palets que ens serveixen com a
+    mòduls centrals en la botiga.
+    Els metalls són en totes les malles metàl·liques i en les
+    estanteries que estan fetes a partir d'un metall corrugat.
+    I finalment els vidres, estan col·locats de manera de crear
+    un revestiment en alguns trams de paret.
+    
+    La il·luminació es tracta d'algun llum neó i downlights en-
+    castats al sostre, que es pot modificar la seva forma, de
+    
+    manera que apunti bé als llocs on sigui interessant.
+    Finalment, la instal·lació de climatització es tractaria d'una
+    instal·lació vista amb un acabat pintat de negre perquè
+    passi desapercebut amb el sostre, ja que està pintat igual.
+    
+    
+    
+    `,
+    category: "interior",
+    pdf: ".../redskate-store/REDSKATE.pdf",
+    color: "#f27676",
+  },
+  {
+    id: "10",
+    title: "Un mar de flors",
+    mainImage: "../rosesreus/real/real-rosesreus-1.png",
+
+    realImages: [
+      "../rosesreus/real/real-rosesreus-1.png",
+      "../rosesreus/real/real-rosesreus-2.png",
+      "../rosesreus/real/real-rosesreus-3.png",
+      "../rosesreus/real/real-rosesreus-4.png",
+      "../rosesreus/real/real-rosesreus-5.png",
+    ],
+    tecnicImages: [
+      "../rosesreus/planols/planol-rosesreus-1.png",
+      "../rosesreus/planols/planol-rosesreus-2.png",
+      "../rosesreus/planols/planol-rosesreus-3.png",
+      "../rosesreus/planols/planol-rosesreus-4.png",
+    ],
+
+    description: `
+    "La mayoría de la gente, en el ajetreo de la ciudad, no tiene tiempo de mirar una flor.
+
+Quiero saber si esto es más un deseo o un impedimento".
+Instal·lació en la que utilitzem tubs d’acer juntament amb flors de tonalitat
+blavosa per seguir amb l’estètica dels bancs. El tub portaria un tope per poder-lo omplir
+d’aigua en el cas de que la flor escollida necessités manteniment. En la nostra instal·lació
+volem utilitzar la lavanda ja que és una flor que aporta calma , no solament perl seu color
+sinò també pel seu olor.
+
+El concepte que volem seguir parteix de la frase esmentada anteriorment. Una descon-
+nexió i reconnexió amb la natura. Basant-nos en las ones del mar.
+
+La flor la utilitzem en un context positiu, volem brindar a l’usuari no solament d’una única
+flor sinò d’un mar de flors.
+Els tubs són d’altures irregulars , reforcen auqest concepte de mar i calma. La gent
+podria atravessar el mar.
+    
+    
+    
+    `,
+    category: "exterior",
+    pdf: ".../rosesreus/ROSESREUS.pdf",
+    color: "#4862aa",
+  },
+  {
+    id: "11",
+    title: "Stand Tesla",
+    mainImage: "../tesla/virtual/virtual-tesla-1.png",
+
+    virtualImages: [
+      "../tesla/virtual/virtual-tesla-1.png",
+      "../tesla/virtual/virtual-tesla-2.png",
+      "../tesla/virtual/virtual-tesla-3.png",
+      "../tesla/virtual/virtual-tesla-4.png",
+      "../tesla/virtual/virtual-tesla-5.png",
+      "../tesla/virtual/virtual-tesla-6.png",
+      "../tesla/virtual/virtual-tesla-7.png",
+    ],
+    tecnicImages: [
+      "../tesla/planols/planol-tesla-1.png",
+      "../tesla/planols/planol-tesla-2.png",
+    ],
+
+    description: `
+    Què és tesla per nosaltres? Com ho hem volgut representar? Què diferència tesla de les altres mar-
+    ques?
+    
+    Per crear el nostre estand ens hem inspirat en les formes lineals dels cotxes i ho hem transformat
+    en aquesta estructura.
+    L'estand té 880m2, fa 37 de llarg per 24 metres d'ample i 8 metres d'alçada.
+    Les zones que hem volgut introduir han sigut les següents:
+    - Zona d'exposicions: consisteix a exposar els 4 models de tesla en quatre colors diferents, per
+    - poder veure'ls en directe.
+    - Zones de reunions: hem habilitat unes taules amb cadires per a possibles clients interessats,
+    - amb possibles dubtes sobre la marca o algun cotxe en concret.
+    - Punt d'informació: Està situat al centre de l'estand per a possibles dubtes o repartició de fulle
+    - tons informatius.
+    - Passadís d'exposició: una zona que consisteix a llegir i admirar plafons i objectes o maquetes
+    - Tesla.
+    - Zona "Crea el teu Tesla": Consisteix en una zona amb pantalles, les quals podràs crear el teu
+    - propi tesla i també poder fer la compra del cotxe.
+    - Zona mecànic: tenim un cotxe desmuntat juntament amb un mecànic entès amb els cotxes Tesla
+    - explicant o resolent dubtes sobre com funcionen aquests cotxes.
+    - Zona projeccions: una zona on hi haurà passant un vídeo relacionat amb el seu no propòsit, posar
+    - plaques solars a les cases dels clients per estalviar energia.
+    - Zona cafeteria: un lloc on poder descansar i prendre alguna cosa.
+    Els materials empleats per realitzar aquest estand són la fusta per fer l'estructura, folrada amb
+    planxes d'alumini on als laterals va una tira de llums led il·luminant i potenciant l'estructura.
+    A terra hem col·locat una catifa grisa que ens serveix per tancar el perímetre de l'estand.
+    Per crear el passadís d'informació hem utilitzat un vidre laminat per enganxar els plafons informatius
+    i per l'altre costat hem utilitzat uns cubs de metacrilat transparent per exposar maquetes, objectes
+    o peces de tesla.
+    Per l'estructura on s'exposen els cotxes hem fet servir el acer, que creiem que resistirà millor al pes
+    dels cotxes.
+    El mobiliari empleat per a completar l'estand han sigut unes pantalles tàctils, per l'interactuació amb
+    les persones i crear interès per la marca. I les taules, cadires, tamborets i pufs, per al descans i
+    interactuació de les persones entre si.
+    
+    `,
+    category: "exterior",
+    pdf: ".../tesla/TESLA.pdf",
+    color: "#951929",
   },
 ];
